a9a4392095b195aa7164709aed1daac5cfce9950
Marios Andreopoulos <opensource@andmarios.com>
2020-07-27 23:04:22 +0300

    Update Lenses to 4.0.0
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- debug-setup.sh
+++ debug-setup.sh
@@ -23,7 +23,7 @@ select yn in "Yes" "No"; do
     case $yn in
         Yes)
             mkdir -p /usr/share/landoop/tools/
-            wget https://archive.landoop.com/third-party/jmxterm/jmxterm-1.0.0-SNAPSHOT-uber.jar \
+            wget https://archive.lenses.io/third-party/jmxterm/jmxterm-1.0.0-SNAPSHOT-uber.jar \
                  -O /usr/share/landoop/tools/jmxterm-1.0.0.jar
             echo "Installed at '/usr/share/landoop/tools/jmxterm-1.0.0.jar'."
             echo "Run with: '/opt/lenses/jre/bin/java -jar /usr/share/landoop/tools/jmxterm-1.0.0.jar'"
@@ -37,7 +37,7 @@ echo "Install lenses-cli to access Lenses from the command line?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes)
-            wget https://archive.landoop.com/tools/lenses-cli/3.0/lenses-cli-linux-amd64-latest.tar.gz \
+            wget https://archive.lenses.io/lenses/4.0/cli/lenses-cli-linux-amd64-latest.tar.gz \
                  -O /tmp/lenses-cli.tgz
             tar xf /tmp/lenses-cli.tgz --strip-components=1 -C /tmp/
             cp /tmp/lenses-cli /usr/local/bin/
