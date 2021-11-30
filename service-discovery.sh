c7b0ce90c875c26e2f6fd74d272de31b051fedf9
George Yord <george.yord@gmail.com>
2021-11-30 21:19:07 +0200

    Remove setup of CC since it will be moved to provisioning flow
    [OPS-2000]

--- service-discovery.sh
+++ service-discovery.sh
@@ -16,19 +16,7 @@ SD_ZOOKEEPER_FILTER=${SD_ZOOKEEPER_FILTER:-}
 SD_ZOOKEEPER_PORT=${SD_ZOOKEEPER_PORT:-2181}
 SD_ZOOKEEPER_JMX_PORT=${SD_ZOOKEEPER_JMX_PORT:-}
 
-# These are comma separated values
-SD_CONNECT_FILTERS=${SD_CONNECT_FILTERS:-}
-SD_CONNECT_NAMES=${SD_CONNECT_NAMES:-default}
-SD_CONNECT_PORTS=${SD_CONNECT_PORTS:-8083}
-SD_CONNECT_JMX_PORTS=${SD_CONNECT_JMX_PORTS:-}
-SD_CONNECT_STATUSES=${SD_CONNECT_STATUSES:-connect-statuses}
-SD_CONNECT_OFFSETS=${SD_CONNECT_OFFSETS:-connect-offsets}
-SD_CONNECT_CONFIGS=${SD_CONNECT_CONFIGS:-connect-configs}
-
-TRUE_REG='^([tT][rR][uU][eE]|[yY]|[yY][eE][sS]|1)$'
-FALSE_REG='^([fF][aA][lL][sS][eE]|[nN]|[nN][oO]|0)$'
-
-rm -f /tmp/sd-broker /tmp/sd-zookeeper /tmp/sd-connect-tmp /tmp/sd-connect /tmp/service-discovery /tmp/service-discovery.log
+rm -f /tmp/sd-broker /tmp/sd-zookeeper /tmp/service-discovery /tmp/service-discovery.log
 
 if [[ -n $SD_BROKER_FILTER ]]; then
     if $FASTDATA_SD \
@@ -56,85 +44,4 @@ if [[ -n $SD_ZOOKEEPER_FILTER ]]; then
     else
         echo "Zookeeper service autodiscovery failed."
     fi
-fi
-
-if [[ -z $SD_CONNECT_FILTERS ]]; then
-    exit 0
-fi
-
-IFS="," read -r -a c_filters <<< "$SD_CONNECT_FILTERS"
-IFS="," read -r -a c_names <<< "$SD_CONNECT_NAMES"
-IFS="," read -r -a c_ports <<< "$SD_CONNECT_PORTS"
-IFS="," read -r -a c_jmx_ports <<< "$SD_CONNECT_JMX_PORTS"
-IFS="," read -r -a c_statuses <<< "$SD_CONNECT_STATUSES"
-IFS="," read -r -a c_configs <<< "$SD_CONNECT_CONFIGS"
-IFS="," read -r -a c_offsets <<< "$SD_CONNECT_OFFSETS"
-
-c_length=${#c_filters[@]}
-c_port=""
-c_jmx_port=""
-function check_length(){
-    # Connect ports can have length 1
-    if [[ $2 == SD_CONNECT_PORTS ]]; then
-        if ! [[ $SD_CONNECT_PORTS =~ .*,.* ]]; then
-            c_port=$SD_CONNECT_PORTS
-            return
-        fi
-        # Connect jmx ports can have lengths 0 and 1
-    elif [[ $2 == SD_CONNECT_JMX_PORTS ]]; then
-        if [[ -z $1 ]]; then return
-        elif ! [[ $SD_CONNECT_JMX_PORTS =~ .*,.* ]]; then
-            c_jmx_port=$SD_CONNECT_JMX_PORTS
-            return
-        fi
-    fi
-    if [[ $c_length != $1 ]]; then
-        echo "DEBUG c_length: $c_length, 1: $1"
-
-        echo "SD_CONNECT_FILTERS length different than $2 length."
-        echo "Won't perform service discovery for connect."
-        exit 0
-    fi
-}
-# Check if all arrays have the same length. For ports we also accept length 1. For jmx_ports we also accept lengths 0 and 1.
-check_length ${#c_names[@]} SD_CONNECT_NAMES
-check_length ${#c_ports[@]} SD_CONNECT_PORTS
-check_length ${#c_jmx_ports[@]} SD_CONNECT_JMX_PORTS
-check_length ${#c_statuses[@]} SD_CONNECT_STATUSES
-check_length ${#c_configs[@]} SD_CONNECT_CONFIGS
-check_length ${#c_offsets[@]} SD_CONNECT_OFFSETS
-cp_length=${c_ports[@]}
-cjp_length=${c_jmx_ports[@]}
-for index in "${!c_filters[@]}"; do
-    c_port_final=""
-    c_jmx_port_final=""
-    if [[ -z $c_port ]]; then
-        c_port_final="${c_ports[index]}"
-    else
-        c_port_final=$c_port
-    fi
-    if [[ -n $c_jmx_port ]]; then
-        c_jmx_port_final="-jmx-port $c_jmx_port"
-    elif ! [[ -z $SD_CONNECT_JMX_PORTS ]]; then
-            c_jmx_port_final="-jmx-port ${c_jmx_ports[index]}"
-    fi
-    echo $FASTDATA_SD \
-           -mode=connect -port $c_port_final $c_jmx_port_final \
-           -cluster-name ${c_names[index]} -statuses ${c_statuses[index]} -configs ${c_configs[index]} -offsets ${c_offsets[index]} \
-           addrs $SD_CONFIG \
-           ${c_filters[index]}
-    if $FASTDATA_SD \
-           -mode=connect -port $c_port_final $c_jmx_port_final \
-           -cluster-name ${c_names[index]} -statuses ${c_statuses[index]} -configs ${c_configs[index]} -offsets ${c_offsets[index]} \
-           addrs $SD_CONFIG \
-           ${c_filters[index]} > /tmp/sd-connect-tmp 2>>/tmp/service-discovery.log; then
-        cat /tmp/sd-connect-tmp >> /tmp/sd-connect
-        echo -n "," >> /tmp/sd-connect
-    else
-        echo "Connect '${c_names[index]}' service autodiscovery failed."
-    fi
-done
-if [[ -f /tmp/sd-connect ]]; then
-    echo "export LENSES_CONNECT_CLUSTERS='[$(cat /tmp/sd-connect | sed -e 's/,$//')]'" | tee -a /tmp/service-discovery
-    echo
 fi
